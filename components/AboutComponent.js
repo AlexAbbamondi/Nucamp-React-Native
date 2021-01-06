ede02c361b2458c5d321b240f2c816e226d19995
Alex Abbamondi <alex.abbamondi@gmail.com>
2021-01-06 14:13:06 -0500

    navigation

---
+++ components/AboutComponent.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { ScrollView, FlatList } from "react-native";
+import { Card, ListItem } from "react-native-elements";
+import { PARTNERS } from "../shared/partners";
+import Mission from "./Mission";
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      partners: PARTNERS,
+    };
+  }
+
+  static navigationOptions = {
+    title: "About Us",
+  };
+
+  render() {
+    const renderPartner = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          leftAvatar={{ source: require("./images/bootstrap-logo.png") }}
+        />
+      );
+    };
+
+    return (
+      <ScrollView>
+        <Mission />
+        <Card title="Community Partners">
+          <FlatList
+            data={this.state.partners}
+            renderItem={renderPartner}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </Card>
+      </ScrollView>
+    );
+  }
+}
+
+export default About;
